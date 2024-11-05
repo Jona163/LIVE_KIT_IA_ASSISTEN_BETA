ccdc58c96a6ccc0ccb0fa7cbddf88417fb1e897b
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-04 20:01:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, 
 from api import AssistantFnc
+
+load_dotenv()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
