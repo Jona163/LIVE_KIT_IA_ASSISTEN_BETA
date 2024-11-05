3ad9f97a3aadc853242d6e2a42f6580da0075f1e
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-04 20:01:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ load_dotenv()
 
 async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
+        ),
+    )
