eb95559a9dc1bd2b8c0b7d524e0590d4eee232e5
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-04 20:00:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import asyncio
 
+from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
+from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.plugins import openai, 
