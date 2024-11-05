4487eb18e0af32c1f343861e72f70a7f4ce14698
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-04 20:01:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, 
+from api import AssistantFnc
